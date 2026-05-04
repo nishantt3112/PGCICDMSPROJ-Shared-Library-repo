7222e00c798b36dd95e9f9cee7e330b077882dfc
nishant <nishant@gmail.com>
2026-05-04 12:47:48 +0530

    updated builandtest

--- vars/buildAndTest.groovy
+++ vars/buildAndTest.groovy
@@ -1,53 +1,94 @@
-// def call(config){
-//         dir(config.servicePath){
-//         sh """
-//         ${config.buildCmd}
-//          """
-//         sh """
-//         ${config.testCmd}
-//         """
+// // def call(config){
+// //         dir(config.servicePath){
+// //         sh """
+// //         ${config.buildCmd}
+// //          """
+// //         sh """
+// //         ${config.testCmd}
+// //         """
+// //         }
+// //  }
+
+// def call(Map config = [:]) {
+
+//     dir(config.servicePath) {
+
+//         stage('Build & Test') {
+//             steps {
+//                 script {
+
+//                     sh """
+//                         set -e
+
+//                         echo "=== BUILD START ==="
+
+//                         ${config.buildAndTestCmd?.trim() ?: '''
+//                             chmod +x ./gradlew
+
+//                             ./gradlew dependencies --no-daemon || true
+
+//                             ./gradlew clean build \
+//                                 -x verifyGoogleJavaFormat \
+//                                 -x distZip \
+//                                 -x distTar \
+//                                 --no-daemon
+//                         '''}
+
+//                         echo "=== BUILD END ==="
+//                     """
+
+//                 }
+//             }
 //         }
-//  }
+
+//         stage('JUnit Reports') {
+//             steps {
+//                 script {
+//                     junit allowEmptyResults: true,
+//                           testResults: config.jUnitReportPath ?: 'build/test-results/test/*.xml'
+//                 }
+//             }
+//         }
+//     }
+// }
+
+///cant use groovy script in shared lib helper 
 
 def call(Map config = [:]) {
 
     dir(config.servicePath) {
 
         stage('Build & Test') {
-            steps {
-                script {
 
-                    sh """
-                        set -e
+            sh """
+                set -e
 
-                        echo "=== BUILD START ==="
+                echo "=== BUILD START ==="
 
-                        ${config.buildAndTestCmd?.trim() ?: '''
-                            chmod +x ./gradlew
+                ${config.buildAndTestCmd?.trim() ?: '''
+                    chmod +x ./gradlew
 
-                            ./gradlew dependencies --no-daemon || true
+                    ./gradlew dependencies --no-daemon || true
 
-                            ./gradlew clean build \
-                                -x verifyGoogleJavaFormat \
-                                -x distZip \
-                                -x distTar \
-                                --no-daemon
-                        '''}
+                    ./gradlew clean build \
+                        -x verifyGoogleJavaFormat \
+                        -x distZip \
+                        -x distTar \
+                        --no-daemon
+                '''}
 
-                        echo "=== BUILD END ==="
-                    """
-
-                }
-            }
+                echo "=== BUILD END ==="
+            """
         }
 
         stage('JUnit Reports') {
-            steps {
-                script {
-                    junit allowEmptyResults: true,
-                          testResults: config.jUnitReportPath ?: 'build/test-results/test/*.xml'
-                }
-            }
+
+            junit(
+                allowEmptyResults: true,
+                testResults:
+                    config.jUnitReportPath
+                    ?: 'build/test-results/test/*.xml'
+            )
         }
     }
 }
